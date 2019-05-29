99fece4701a3cc289dff3877ac3fe9eab8ace611
JCab09 <jason.cabezuela@stud.h-da.de>
2019-05-29 19:48:18 +0200

    added partially functioning rosnode client

--- bot_Henry/src/henry/clients/events/rosnode/config.py
+++ bot_Henry/src/henry/clients/events/rosnode/config.py
@@ -13,41 +13,42 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-@author Jason Cabezuela
+
+This file was created/modified by: Jason Cabezuela
 """
 
 from programy.clients.config import ClientConfigurationData
 from programy.utils.substitutions.substitues import Substitutions
 
-class ConsoleConfiguration(ClientConfigurationData):
+class ROSnodeConfiguration(ClientConfigurationData):
 
     def __init__(self):
-        ClientConfigurationData.__init__(self, "console")
-        self._default_userid = "console"
-        self._prompt = ">>>"
+        ClientConfigurationData.__init__(self, "rosnode")
+        self._default_userid = "rosnode"
+#        self._prompt = ">>>" 
 
     @property
     def default_userid(self):
         return self._default_userid
 
-    @property
-    def prompt(self):
-        return self._prompt
+#    @property
+#    def prompt(self):
+#        return self._prompt
 
     def check_for_license_keys(self, license_keys):
         ClientConfigurationData.check_for_license_keys(self, license_keys)
 
-    def load_configuration_section(self, configuration_file, console, bot_root, subs: Substitutions = None):
-        if console is not None:
-            self._default_userid = configuration_file.get_option(console, "default_userid", missing_value="Console", subs=subs)
-            self._prompt = configuration_file.get_option(console, "prompt", missing_value=">>>", subs=subs)
-            super(ConsoleConfiguration, self).load_configuration_section(configuration_file, console, bot_root, subs=subs)
+    def load_configuration_section(self, configuration_file, rosnode, bot_root, subs: Substitutions = None):
+        if rosnode is not None:
+            self._default_userid = configuration_file.get_option(rosnode, "default_userid", missing_value="Console", subs=subs)
+#            self._prompt = configuration_file.get_option(rosnode, "prompt", missing_value=">>>", subs=subs)
+            super(ROSnodeConfiguration, self).load_configuration_section(configuration_file, rosnode, bot_root, subs=subs)
 
     def to_yaml(self, data, defaults=True):
         if defaults is True:
             data['default_userid'] = "console"
-            data['prompt'] = ">>>"
+#            data['prompt'] = ">>>"
         else:
             data['default_userid'] = self._default_userid
-            data['prompt'] = self._prompt
-        super(ConsoleConfiguration, self).to_yaml(data, defaults)
+#            data['prompt'] = self._prompt
+        super(ROSnodeConfiguration, self).to_yaml(data, defaults)
