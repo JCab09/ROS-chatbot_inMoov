f20ab9e25122dd81e38567d760781c61433f51a3
JCab09 <jason.cabezuela@stud.h-da.de>
2019-06-03 19:12:16 +0200

    more repository fixing..

--- bot_Henry/src/henry/clients/events/rosnode/config.py
+++ bot_Henry/src/henry/clients/events/rosnode/config.py
@@ -21,34 +21,68 @@ from programy.clients.config import ClientConfigurationData
 from programy.utils.substitutions.substitues import Substitutions
 
 class ROSnodeConfiguration(ClientConfigurationData):
-
-    def __init__(self):
+    def __init__(self): 
         ClientConfigurationData.__init__(self, "rosnode")
-        self._default_userid = "rosnode"
-#        self._prompt = ">>>" 
+        print("***clientconfigurationData DONE***")
+        self._default_userid = "rosClient"
+        self._nodename = "chatbot"
+        self._rostopic = "chatOUT"
+        self._subscription = "chatIN"
+        self._rosrate = 10
+        self._queueSize = 10
 
     @property
     def default_userid(self):
         return self._default_userid
+    
+    @property
+    def nodename(self):
+        return self._nodename
 
-#    @property
-#    def prompt(self):
-#        return self._prompt
+    @property
+    def rostopic(self):
+        return self._rostopic
+    
+    @property
+    def subscription(self):
+        return self._subscription
+    
+    @property
+    def rosrate(self):
+        return self._rosrate
+    
+    @property
+    def queueSize(self):
+        return self._queueSize
 
     def check_for_license_keys(self, license_keys):
         ClientConfigurationData.check_for_license_keys(self, license_keys)
 
     def load_configuration_section(self, configuration_file, rosnode, bot_root, subs: Substitutions = None):
         if rosnode is not None:
-            self._default_userid = configuration_file.get_option(rosnode, "default_userid", missing_value="Console", subs=subs)
-#            self._prompt = configuration_file.get_option(rosnode, "prompt", missing_value=">>>", subs=subs)
+            self._default_userid = configuration_file.get_option(rosnode, "default_userid", missing_value="rosClient", subs=subs)
+            self._nodename = configuration_file.get_option(rosnode, "nodename", missing_value="chatbot", subs=subs)
+            self._rostopic = configuration_file.get_option(rosnode, "rostopic", missing_value="chatOUT", subs=subs)
+            self._subscriber = configuration_file.get_option(rosnode, "subscription", missing_value="chatIN", subs=subs)
+            self._rosrate = configuration_file.get_option(rosnode, "rosrate", missing_value=10, subs=subs)
+            self._queueSize = configuration_file.get_option(rosnode, "queue_size", missing_value=10, subs=subs)
             super(ROSnodeConfiguration, self).load_configuration_section(configuration_file, rosnode, bot_root, subs=subs)
 
     def to_yaml(self, data, defaults=True):
         if defaults is True:
-            data['default_userid'] = "console"
-#            data['prompt'] = ">>>"
+            data['default_userid'] = "rosClient"
+            data['nodename'] = "chatbot"
+            data['rostopic'] = "chatOUT"
+            data['subscription'] = "chatIN"
+            data['rosrate'] = 10
+            data['queue_size'] = 10
+            print("to_yaml(default) exec")
         else:
             data['default_userid'] = self._default_userid
-#            data['prompt'] = self._prompt
+            data['nodename'] = self._nodename
+            data['rostopic'] = self._rostopic
+            data['subscription'] = self._subscription
+            data['rosrate'] = self._rosrate
+            data['queue_size'] = self._queueSize
+            print("to_yaml(specific) exec")
         super(ROSnodeConfiguration, self).to_yaml(data, defaults)
