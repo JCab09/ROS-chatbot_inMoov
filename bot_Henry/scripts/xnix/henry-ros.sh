25a49585fda15fe06aa6c2f63f05322fe83f88e3
JCab09 <jason.cabezuela@stud.h-da.de>
2019-06-03 23:18:23 +0200

    added OOB-file for inMoov-gestures

--- bot_Henry/scripts/xnix/henry-ros.sh
+++ bot_Henry/scripts/xnix/henry-ros.sh
@@ -3,6 +3,6 @@
 clear
 
 export PYTHONPATH=../../src:$PYTHONPATH
-export PYTHONPATH=../../program-y/src:$PYTHONPATH
+export PYTHONPATH=../../../program-y/src:$PYTHONPATH
 
 python3 -m henry.clients.events.rosnode.client --config ../../config/xnix/config.ros.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
---
+++ bot_Henry/src/henry/inmoov_gestures/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 23:05:52 2019
+
+@author: jc
+"""
+
+def test_func():
+    print("TEST_FUNC()")
